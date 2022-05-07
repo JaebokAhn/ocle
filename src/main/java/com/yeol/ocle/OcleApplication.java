1acb04aadf164710f69719fc67bf87f9b83bc1af
JaebokAhn <jbahn1106@gmail.com>
2022-05-07 16:41:29 +0900

    init

---
+++ src/main/java/com/yeol/ocle/OcleApplication.java
@@ -0,0 +1,13 @@
+package com.yeol.ocle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcleApplication.class, args);
+	}
+
+}
