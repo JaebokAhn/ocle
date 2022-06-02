b91d5a6c867dc4f025bb98145ef65d19a555c3c3
JaebokAhn <jbahn1106@gmail.com>
2022-06-02 22:26:52 +0900

    사이트구조 개선

---
+++ src/main/java/com/yeol/ocle/controller/admin/ComnMgmtController.java
@@ -0,0 +1,129 @@
+package com.yeol.ocle.controller.admin;
+
+import com.yeol.ocle.comn.cmdto.PageNavInfoDTO;
+import com.yeol.ocle.comn.consts.OcleConst;
+import com.yeol.ocle.comn.exception.BizOcleException;
+import com.yeol.ocle.comn.message.MessageService;
+import com.yeol.ocle.comn.utils.OcleUtils;
+import com.yeol.ocle.controller.admin.modelattr.IntgCodeDTO;
+import com.yeol.ocle.repository.intgcode.IntgCodeValRepository;
+import com.yeol.ocle.service.intgcodemgmt.IntgCodeMgmtService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin/comnmgmt/")
+public class ComnMgmtController {
+    private final String preFix = "/admin/comnmgmt/";
+    private final String postFix = "";
+
+    private final String getViewName(String pageName){
+        return this.preFix + pageName + this.postFix;
+    }
+
+    /**
+     * 메시지서비스
+     */
+    @Autowired
+    private MessageService messageService;
+
+    /**
+     * 공통코드관리서비스
+     */
+    @Autowired
+    private IntgCodeMgmtService intgCodeMgmtService;
+
+    /**
+     * 공통코드값 Repository
+     */
+    @Autowired
+    private IntgCodeValRepository intgCodeValRepository;
+
+
+    /**
+     * 통합코드관리
+     * @param model
+     * @return
+     */
+    @GetMapping("IntgCodeMgmtM")
+    public String IntgCodeMgmtM (
+            Model model,
+            @ModelAttribute("inqyInpt") IntgCodeDTO inqyInpt,
+            @PageableDefault(page = 0, size = 10, sort = "intgCodeId", direction = Sort.Direction.ASC) Pageable pageable) {
+
+        try {
+            log.info("ComnMgmtController.IntgCodeMgmtM START");
+
+
+            if(inqyInpt == null) {
+                inqyInpt = new IntgCodeDTO();
+            }
+
+            Page<Object> intgCodeList = intgCodeMgmtService.selectIntgCodeList(inqyInpt, pageable);
+
+            //검색조건 object
+            model.addAttribute("inqyInpt", inqyInpt);
+
+            //페이징처리정보
+            PageNavInfoDTO pageNavInfo = OcleUtils.getPageNavInfo(intgCodeList);
+            model.addAttribute("pageNavInfo", pageNavInfo);
+
+            //통합코드목록
+            model.addAttribute("intgCodeList", intgCodeList);
+        } catch (BizOcleException e) {
+            model.addAttribute("prcsRsltCode", OcleConst.PRCS_RSLT_CODE_E);
+            model.addAttribute("msgeCode", e.getMessageId());
+            model.addAttribute("msgeCntn", messageService.getMessage(e.getMessageId(), e.getArguments()));
+            return this.getViewName("joinForm");
+        } catch (Exception e) {
+            model.addAttribute("prcsRsltCode", OcleConst.PRCS_RSLT_CODE_E);
+            model.addAttribute("msgeCode", OcleConst.MSGE_CODE_SYSE0001);
+            model.addAttribute("msgeCntn", messageService.getMessage(OcleConst.MSGE_CODE_SYSE0001));
+            return this.getViewName("joinForm");
+        }
+
+        return this.getViewName("IntgCodeMgmtM");
+    }
+
+    /**
+     * 통합코드값목록 조회
+     * @param model
+     * @param intgCodeId
+     * @return
+     */
+    @PostMapping("IntgCodeMgmtT01")
+    public String IntgCodeMgmtT01(
+            Model model,
+            @RequestParam(value = "intgCodeId") String intgCodeId
+    ) {
+        log.info("intgCodeId : {} ", intgCodeId);
+
+        //통합코드값 목록
+        List<Object> intgCodeValList = intgCodeValRepository.findByIntgCodeId(intgCodeId);
+
+        model.addAttribute("intgCodeValList", intgCodeValList);
+        return this.getViewName("IntgCodeMgmtT01");
+    }
+
+    /**
+     * 통합메시지관리
+     * @param model
+     * @return
+     */
+    @GetMapping("IntgMsgeMgmtM")
+    public String IntgMsgeMgmtM(Model model) {
+
+        return this.getViewName("IntgMsgeMgmtM");
+    }
+}
+
