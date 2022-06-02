136f45daf10aa90d008b7a285b62e9f986b85f52
JaebokAhn <jbahn1106@gmail.com>
2022-06-02 20:06:32 +0900

    빈카피

--- src/main/java/com/yeol/ocle/comn/utils/BeanCopyUtils.java
+++ src/main/java/com/yeol/ocle/comn/utils/BeanCopyUtils.java
@@ -2,6 +2,11 @@ package com.yeol.ocle.comn.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.util.SerializationUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Slf4j
 public abstract class BeanCopyUtils extends BeanUtils {
@@ -20,6 +25,23 @@ public abstract class BeanCopyUtils extends BeanUtils {
                 log.warn("copyList Exception ={}", var4);
                 return (T) target;
             }
+        }
+    }
+
+    public static <T> List<T> copyDtoList(List<?> sourceList, Class<T> targetType) {
+        if (sourceList == null) {
+            log.info("from source instance is null");
+            return null;
+        } else {
+            List<T> targetList = new ArrayList();
+            Iterator var3 = sourceList.iterator();
+
+            while(var3.hasNext()) {
+                Object element = var3.next();
+                targetList.add(copyDto(element, targetType));
+            }
+
+            return targetList;
         }
     }
 }
